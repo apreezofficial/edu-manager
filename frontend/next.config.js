2b5ecf1f80d646549636f85ff641fb1b72d735cb
apreezofficial <apreezofficial@gmail.com>
2026-05-29 08:28:52 +0100

    fixed stray double‑quote

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  // Allow ESM packages like supports-color to be processed correctly
+  webpack: (config, { isServer }) => {
+    // Treat supports-color as a regular JS module (disable ESM external handling)
+    config.module.rules.push({
+      test: /supports-color/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
